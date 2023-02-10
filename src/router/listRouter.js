de0121c45daf4b62651f3ae419773b0e1246a995
bilguun082 <bilguune060829@gmail.com>
2023-02-10 17:47:58 +0800

    pagination

--- src/router/listRouter.js
+++ src/router/listRouter.js
@@ -14,6 +14,6 @@ listRouter.get("/", getLists);
 listRouter.post("/create", createList);
 listRouter.get("/user/:id", getUserLink);
 listRouter.get("/:id", getLink);
-listRouter.get("/page/:limit", getHistory);
+listRouter.get("/:page/:limit", getHistory);
 
 module.exports = listRouter;
